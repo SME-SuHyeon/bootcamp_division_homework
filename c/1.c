5589fa12ee7bbd14f737c8c065a8b6c17ea867b9
오수현 <kkehohk55@naver.com>
2024-03-06 01:10:38 +0900

    해달 채점 - c

--- c/1.c
+++ c/1.c
@@ -1,12 +1,19 @@
-/*
-    main 함수 안의 내용만 수정해주세요.
-    수정할 경우, 자동 채점 프로그램이 제대로 동작하지 않을 가능성이 있습니다.
-*/
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 int main() {
-    // 이곳에 코드를 작성해주세요!
+
+    int a;
+
+    scanf("%d", &a);
+
+    printf("%d", a % 10);
+    a /= 10;
+
+    printf("%d", a % 10);
+    a /= 10;
+
+    printf("%d", a);
 
     return 0;
 }
